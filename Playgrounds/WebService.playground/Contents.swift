5837427866d7f8125156197181fac345d9433f1f
Miguel Jardón <migueljp83@gmail.com>
2017-10-07 19:27:17 +0200

    Fin 2 dia.

--- Playgrounds/WebService.playground/Contents.swift
+++ Playgrounds/WebService.playground/Contents.swift
@@ -6,11 +6,20 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 let disposeBag = DisposeBag()
-let assembly = CoreAssembly()
+let assembly = CoreAssembly(navigationController: UINavigationController())
 let webService = assembly.webServiceAssembly.webService
+
+
+let endpoint = Endpoint.configuration
+let urlRequest = endpoint.request(with: URL(string: "https//api.themoviedb.org/3")!,adding: ["api_key": "fistro"])
+print(urlRequest)
+print(urlRequest.httpMethod!)
 
 // Load configuration
 
 webService.load(Configuration.self, from: .configuration)
 	.subscribe(onNext: { print($0.images.baseURL) })
 	.disposed(by: disposeBag)
+
+
+
---
+++ TMDbCore/Featured/Domain/Page.swift
@@ -0,0 +1,24 @@
+//
+//  Page.swift
+//  TMDbCore
+//
+//  Created by MIGUEL JARDÓN PEINADO on 7/10/17.
+//  Copyright © 2017 Guille Gonzalez. All rights reserved.
+//
+
+import Foundation
+
+
+struct Page<T: Decodable>: Decodable {
+    let page: Int
+    let totalPages: Int
+    let totalResults: Int
+    let results: [T]
+    
+    private enum CodingKeys: String, CodingKey {
+        case page
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+        case results
+    }
+}
